9f738262754049ed73f25fa3d90f4a6145a18c2b
lixia <lixiaofeng1115@outlook.com>
2025-07-15 00:20:41 +0800

    匹配

--- Assets/Scripts/MemoryCard.cs
+++ Assets/Scripts/MemoryCard.cs
@@ -1,18 +1,18 @@
-using System;
 using UnityEngine;
 
 public class MemoryCard : MonoBehaviour
 {
     private GameObject _cardBack;
-    
+    private SceneController _sceneController;
     private int _id;
     public int ID => _id;
     private SpriteRenderer _spriteRenderer;
-
+    private bool _matchSuccess; // 匹配成功
 
     private void Start()
     {
         _cardBack = transform.GetChild(0).gameObject;
+        _sceneController = FindFirstObjectByType<SceneController>();
     }
 
     public void SetCard(int id, Sprite image)
@@ -26,12 +26,27 @@ public class MemoryCard : MonoBehaviour
     // 点击卡片
     private void OnMouseDown()
     {
-        _cardBack.SetActive(false);
+        // 如果已匹配成功过，则无法再被点击
+        if (_matchSuccess)
+            return;
+        
+        // 点击2张不同的卡片后，不能再点击其他卡片
+        if (_sceneController.CanReveal)
+        {
+            _cardBack.SetActive(false);
+            _sceneController.CardReveal(this);
+        }
     }
 
     // 翻回卡片
     public void Cover()
     {
         _cardBack.SetActive(true);
+    }
+    
+    // 匹配成功
+    public void MatchSuccess()
+    {
+        _matchSuccess = true;
     }
 }
--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -11,13 +12,16 @@ public class SceneController : MonoBehaviour
     public float originalY = -2.5f;
     public GameObject cardPrefab;
     public Sprite[] images;
-    
-    
+
+    private MemoryCard _first; // 第1次点击的卡片
+    private MemoryCard _second; // 第2次点击的卡片
+
+    public bool CanReveal => _second == null;
+
     private void Start()
     {
         int[] nums = {0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
         Shuffle(nums);
-        print(string.Join(',', nums));
         
         for (int i = 0; i < gridRows; i++)
             for (int j = 0; j < gridCols; j++)
@@ -36,5 +40,46 @@ public class SceneController : MonoBehaviour
             int idx = Random.Range(i, nums.Length);
             (nums[i], nums[idx]) = (nums[idx], nums[i]);
         }
+    }
+
+    // 点击卡片
+    public void CardReveal(MemoryCard card)
+    {
+        if (_first == null)
+        {
+            _first = card;
+            return;
+        }
+
+        // 如果第1次和第2次点击了同一张卡片
+        if (_first == card)
+        {
+            _first.Cover();
+            _first = null;
+            return;
+        }
+        
+        _second = card;
+        StartCoroutine(CheckMatch());
+    }
+
+    private IEnumerator CheckMatch()
+    {
+        if (_first.ID == _second.ID)
+        {
+            // 匹配成功
+            _first.MatchSuccess();
+            _second.MatchSuccess();
+        }
+        else
+        {
+            // 匹配失败，1.5秒后2张牌都翻转
+            yield return new WaitForSeconds(1.5f);
+            _first.Cover();
+            _second.Cover();
+        }
+
+        _first = null;
+        _second = null;
     }
 }
